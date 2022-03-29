26dca261d8e26bec77f74dbac95539668cf11154
Melvin Mallari <melvintmallari@gmail.com>
2022-03-28 22:06:20 -0700

    finish adding the rest of the visitor methods for the resolver

--- src/resolver.ts
+++ src/resolver.ts
@@ -1,16 +1,32 @@
 import {
   AssignExpr,
+  BinaryExpr,
   BlockStmt,
+  CallExpr,
+  ExpressionStmt,
   ExprVisitor,
+  FunctionStmt,
+  GroupingExpr,
+  IfStmt,
+  LiteralExpr,
+  LogicalExpr,
+  PrintStmt,
+  ReturnStmt,
   StmtVisitor,
+  UnaryExpr,
   VariableExpr,
   VarStmt,
+  WhileStmt,
 } from "./ast";
 import { Interpreter } from "./interpreter";
 import { Stmt, Expr } from "./ast";
 import { error } from "./lox";
 import Token from "./token";
 
+// the resolver is semantically analyzes the code.
+// this means that it inspects the user's program, finds every variable mentioned
+// & figures out which declaration each refers to
+// our analysis will resolve variable bindings — we'll know what expression that variable is & what variable that is
 class Resolver implements ExprVisitor<void>, StmtVisitor<void> {
   private readonly interpreter: Interpreter;
   private readonly scopes: Map<string, boolean>[] = [];
@@ -45,8 +61,69 @@ class Resolver implements ExprVisitor<void>, StmtVisitor<void> {
   }
 
   visitAssignExpr(expr: AssignExpr): void {
+    // resolve the expression in case it contains references to other variables
     this.resolveExpression(expr.value);
+    // resolve the variable that it's being assigned to
     this.resolveLocal(expr, expr.name);
+  }
+
+  visitFunctionStmt(stmt: FunctionStmt): void {
+    this.declare(stmt.name);
+    // unlike variables, define eagerly so that the function may refer to itself recursively
+    this.define(stmt.name);
+    this.resolveFunction(stmt);
+  }
+
+  visitExpressionStmt(stmt: ExpressionStmt): void {
+    this.resolveExpression(stmt.expression);
+  }
+
+  visitIfStmt(stmt: IfStmt): void {
+    this.resolveExpression(stmt.condition);
+    this.resolveStatement(stmt.thenBranch);
+    if (stmt.elseBranch !== null) this.resolveStatement(stmt.elseBranch);
+  }
+
+  visitPrintStmt(stmt: PrintStmt): void {
+    this.resolveExpression(stmt.expression);
+  }
+
+  visitReturnStmt(stmt: ReturnStmt): void {
+    if (stmt.value !== null) {
+      this.resolveExpression(stmt.value);
+    }
+  }
+
+  visitWhileStmt(stmt: WhileStmt): void {
+    this.resolveExpression(stmt.condition);
+    this.resolveStatement(stmt.body);
+  }
+
+  visitBinaryExpr(expr: BinaryExpr): void {
+    this.resolveExpression(expr.left);
+    this.resolveExpression(expr.right);
+  }
+
+  visitCallExpr(expr: CallExpr): void {
+    this.resolveExpression(expr.callee);
+    for (const arg of expr.args) {
+      this.resolveExpression(arg);
+    }
+  }
+
+  visitGroupingExpr(expr: GroupingExpr): void {
+    this.resolveExpression(expr.expression);
+  }
+
+  visitLiteralExpr(_: LiteralExpr): void {}
+
+  visitLogicalExpr(expr: LogicalExpr): void {
+    this.resolveExpression(expr.left);
+    this.resolveExpression(expr.right);
+  }
+
+  visitUnaryExpr(expr: UnaryExpr): void {
+    this.resolveExpression(expr.right);
   }
 
   resolve(statements: Stmt[]): void {
@@ -61,6 +138,16 @@ class Resolver implements ExprVisitor<void>, StmtVisitor<void> {
         return;
       }
     }
+  }
+
+  private resolveFunction(func: FunctionStmt) {
+    this.beginScope();
+    for (const param of func.params) {
+      this.declare(param);
+      this.define(param);
+    }
+    this.resolve(func.body);
+    this.endScope();
   }
 
   private resolveStatement(stmt: Stmt) {
